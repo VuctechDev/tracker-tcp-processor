ffc6b324ec7b23e9aed1110853ef0ec0f8bcb808
stefanvucic <stefan.vucic@neotechsolutions.org>
2025-05-12 21:49:50 +0200

    test2345794op9434hpe3btlwisstwpk

--- src/baza.ts
+++ src/baza.ts
@@ -34,9 +34,9 @@ export const query = <T>(
 
 export const insertInDB = (data: any) => {
   const device_id = "0861261021070616";
-  const latitude = data.latitude;
+  const latitude = data.latitude??.toFixed(5;
   const longitude = data.longitude?.toFixed(5);
-  const battery = data.speed?.toFixed(5);
+  const battery = 89;
   const time = new Date();
   const formattedTime = time.toISOString().slice(0, 19).replace("T", " ");
   query(
