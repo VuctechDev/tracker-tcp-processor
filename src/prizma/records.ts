f9e4ab9148fb74d917d2a5aa8c3e441cf862c039
stefanvucic <stefan.vucic@neotechsolutions.org>
2025-05-16 11:31:20 +0200

    test2345678

--- src/prizma/records.ts
+++ src/prizma/records.ts
@@ -1,13 +1,13 @@
-import { Prisma } from "@prisma/client";
 import prisma from ".";
 import { GpsPacket } from "../decoders/gps";
+import { Decimal } from "decimal.js";
 
 export const insert = async (data: GpsPacket) => {
   await prisma.records.create({
     data: {
       deviceId: data.deviceId,
-      lat: new Prisma.Decimal(`${data?.latitude}`),
-      long: new Prisma.Decimal(`${data?.longitude}`),
+      lat: new Decimal(`${data?.latitude}`),
+      long: new Decimal(`${data?.longitude}`),
       speed: data?.speed,
       createdAt: data?.dateTimeUTC ? new Date(data?.dateTimeUTC) : undefined,
     },
