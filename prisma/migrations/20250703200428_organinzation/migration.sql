9875133fc5118658b4cca1528a10dc4c16093d19
stefanvucic <stefan.vucic@neotechsolutions.org>
2025-07-04 01:56:49 +0200

    org added2453

--- prisma/migrations/20250703200428_organinzation/migration.sql
+++ prisma/migrations/20250703200428_organinzation/migration.sql
@@ -1,40 +1,50 @@
-/*
-  Warnings:
-
-  - Added the required column `organizationId` to the `devices` table without a default value. This is not possible if the table is not empty.
-
-*/
-
--- CreateTable
+-- 1. Create organizations table
 CREATE TABLE "organizations" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL
 );
 
-INSERT INTO "organizations" ("id", "name")
-VALUES (1, 'Default')
-ON CONFLICT ("id") DO NOTHING;
+-- 2. Insert Default organization (auto id)
+INSERT INTO "organizations" ("name")
+VALUES ('Default')
+ON CONFLICT ("name") DO NOTHING;
 
--- AlterTable
-ALTER TABLE "devices" ADD COLUMN     "organizationId" INTEGER NOT NULL DEFAULT 1;
+-- 3. Alter devices: add organizationId as nullable
+ALTER TABLE "devices"
+ADD COLUMN "organizationId" INTEGER;
 
--- CreateTable
+-- 4. Backfill existing devices with Default organization id
+UPDATE "devices"
+SET "organizationId" = (
+  SELECT "id" FROM "organizations" WHERE "name" = 'Default'
+)
+WHERE "organizationId" IS NULL;
+
+-- 5. Alter devices: set organizationId to NOT NULL
+ALTER TABLE "devices"
+ALTER COLUMN "organizationId" SET NOT NULL;
+
+-- 6. Create users table
 CREATE TABLE "users" (
-    "id" SERIAL NOT NULL,
+    "id" SERIAL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "code" TEXT NOT NULL,
-    "organizationId" INTEGER NOT NULL DEFAULT 1,
-
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+    "organizationId" INTEGER NOT NULL
 );
 
--- CreateIndex
+-- 7. Create unique index on users.code
 CREATE UNIQUE INDEX "users_code_key" ON "users"("code");
 
--- AddForeignKey
-ALTER TABLE "devices" ADD CONSTRAINT "devices_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- 8. Add foreign key constraint on devices.organizationId
+ALTER TABLE "devices"
+ADD CONSTRAINT "devices_organizationId_fkey"
+FOREIGN KEY ("organizationId")
+REFERENCES "organizations"("id")
+ON DELETE RESTRICT ON UPDATE CASCADE;
 
--- AddForeignKey
-ALTER TABLE "users" ADD CONSTRAINT "users_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- 9. Add foreign key constraint on users.organizationId
+ALTER TABLE "users"
+ADD CONSTRAINT "users_organizationId_fkey"
+FOREIGN KEY ("organizationId")
+REFERENCES "organizations"("id")
+ON DELETE RESTRICT ON UPDATE CASCADE;
