8b8cadd803900c855f6075c77d3f9789aed9ca8c
stefanvucic <stefan.vucic@neotechsolutions.org>
2025-07-04 02:03:08 +0200

    org added2453

--- prisma/migrations/20250703200428_organinzation/migration.sql
+++ prisma/migrations/20250703200428_organinzation/migration.sql
@@ -14,37 +14,4 @@ ALTER TABLE "devices"
 ADD COLUMN "organizationId" INTEGER;
 
 -- 4. Backfill existing devices with Default organization id
-UPDATE "devices"
-SET "organizationId" = (
-  SELECT "id" FROM "organizations" WHERE "name" = 'Default'
-)
-WHERE "organizationId" IS NULL;
 
--- 5. Alter devices: set organizationId to NOT NULL
-ALTER TABLE "devices"
-ALTER COLUMN "organizationId" SET NOT NULL;
-
--- 6. Create users table
-CREATE TABLE "users" (
-    "id" SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
-    "organizationId" INTEGER NOT NULL
-);
-
--- 7. Create unique index on users.code
-CREATE UNIQUE INDEX "users_code_key" ON "users"("code");
-
--- 8. Add foreign key constraint on devices.organizationId
-ALTER TABLE "devices"
-ADD CONSTRAINT "devices_organizationId_fkey"
-FOREIGN KEY ("organizationId")
-REFERENCES "organizations"("id")
-ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- 9. Add foreign key constraint on users.organizationId
-ALTER TABLE "users"
-ADD CONSTRAINT "users_organizationId_fkey"
-FOREIGN KEY ("organizationId")
-REFERENCES "organizations"("id")
-ON DELETE RESTRICT ON UPDATE CASCADE;
